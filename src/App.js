4775e67c87c736c3c5914fed1024ab00a6d70685
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-04-11 13:40:31 -0500

    Make some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+//@flow
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './Chat/message_1';
+
+type Multimedia = {
+  uri: string,
+  creation_timestamp: number
+}
+
+type MessageType = {
+  audio_files?: Array<Multimedia>,
+  content?: string,
+  photos?: Array<Multimedia>,
+  sender_name: string,
+  timestamp_ms: number
+}
+
+type MessageProps = {
+  message: MessageType
+}
+
+function toDateFormat(timestampInMs: number): string{
+  const date = new Date(timestampInMs);
+  const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  const year = date.getFullYear();
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  return day + '/' + month + '/' + year + ' at ' + hour + ':' + min + ':' + sec ;
+}
+
+function Message({ message }: MessageProps) {
+  const { audio_files, content, sender_name, timestamp_ms } = message;
+  let actualContent = content;
+  if(actualContent == null) {
+    actualContent = audio_files == null ? "fotos" : "audio";
+  }
+  return (
+    <div>
+      {sender_name} sent: {actualContent} - the {toDateFormat(timestamp_ms)}
+    </div>
+  );
+}
 
 function App() {
+  const messages = data['messages'].reverse().filter(message => message['content'] != null);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { messages.map((message) => <Message message={message} key={message['timestamp_ms']}/>) }
       </header>
     </div>
   );
